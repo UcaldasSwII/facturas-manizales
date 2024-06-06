a162cafe43ce85b045c7a30a1c7871531cf020f0
ERLEY <erley.1701920254@ucaldas.edu.co>
2024-06-06 17:11:31 -0500

    Rama principal

---
+++ modulos/auth/auth.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, Depends, HTTPException
+from passlib.hash import bcrypt
+from sqlalchemy.orm import Session
+from .jwt_manage import signJWT
+from database import SessionLocal
+from .users import User, create_user
+
+auth_router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@auth_router.post("/user/signup")
+async def create_user_endpoint(user: User, db: Session = Depends(get_db)):
+    created_user = create_user(db, user)
+    return signJWT(created_user.username)
+
+@auth_router.post("/user/login")
+async def user_login(user: User, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.username == user.username).first()
+    if db_user and bcrypt.verify(user.password, db_user.hashed_password):
+        return signJWT(db_user.username)
+    return {"error": "Invalid login credentials!"}
