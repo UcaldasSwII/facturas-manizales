0b764799b0b159e6b02f8ea80242375ccc6cdc16
korpi-zip <santiescoal@gmail.com>
2024-06-06 23:04:11 -0500

    add secrets to cicd

---
+++ test_servicios.py
@@ -0,0 +1,36 @@
+"""
+Test file for pytest to test the module Servicios that have a model with:
+    codigo_suscripcion = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, index=True)
+    tipo = Column(Enum(ListaServicios))
+    usuario_id = Column(Integer, ForeignKey('users.id'))
+"""
+from fastapi.testclient import TestClient
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from config.db import Base, get_db
+from main import app
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+app.dependency_overrides[get_db] = override_get_db
+
+client = TestClient(app)
+
+
+
