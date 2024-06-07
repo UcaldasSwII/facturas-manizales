4b86ab0eadb4fe0ba10a9b024d038fe084c04488
korpi-zip <santiescoal@gmail.com>
2024-06-06 22:29:05 -0500

    test_auth fix

--- test_auth.py
+++ test_auth.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from config.db import Base, get_db
 from main import app
 
+
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
@@ -24,14 +25,16 @@ app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
+
 def test_register_user():
     response = client.post(
         "/users/",
         json={
             "username": "testuser",
             "email": "test@gmail.com",
-            "name" : "testname",
-            "password": "testpassword",
+            "id": 1,
         })
     assert response.status_code == 201
     assert response.json()['message'] == 'User created'
+
+
