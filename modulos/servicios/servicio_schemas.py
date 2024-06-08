6b3ffd92e019a8a9489b73e6e674dad6e868aa66
ERLEY <erley.1701920254@ucaldas.edu.co>
2024-06-07 21:25:03 -0500

    Refacto, despliegue y test

--- modulos/servicios/servicio_schemas.py
+++ modulos/servicios/servicio_schemas.py
@@ -3,11 +3,6 @@ from typing import Optional
 from enum import Enum
 from modulos.servicios.servicio_model import ListaServicios
 
-class ListaServicios(str, Enum):
-    AGUAS_DE_MANIZALES = "AGUAS DE MANIZALES"
-    EFIGAS = "EFIGAS"
-    CHEC = "CHEC"
-
 class ServicioBase(BaseModel):
     nombre: str
     tipo: ListaServicios
--- test_auth.py
+++ test_auth.py
@@ -30,6 +30,7 @@ def test_register_user():
     response = client.post(
         "/users/",
         json={
+            "id": 1,
             "username": "yo",
             "email": "yo@mail.com",
             "name": "yo",
@@ -37,5 +38,45 @@ def test_register_user():
     })
     assert response.status_code == 201
     assert response.json()['message'] == 'User created'
+
+def test_login_user():
+    response = client.post(
+        "/auth/login",
+        json={
+            "username": "yo",
+            "password": "123"
+    })
+    assert response.status_code == 200
+    assert 'token' in response.json()
+
+def test_get_all_users():
+    response = client.get(
+        "/users/",
+    )
+    assert response.status_code == 200
+    assert len(response.json()) > 0
+
+def test_register_service():
+    response = client.post(
+        "/servicios/",
+        json={
+        "nombre": "prueba",
+        "tipo": "AGUAS DE MANIZALES",
+        "usuario_id": 1
+    })
+    assert response.status_code == 200
+
+def test_register_factura():
+    response = client.post(
+        "/facturas/",
+        json={
+            "consumo": 200000,
+            "costo": 30000,
+            "estado": "pendiente",
+            "fecha_pago": "2024-06-07",
+            "servicio_id": 1
+        })
+    assert response.status_code == 200
+
 
 
