a13dda479e94ece3a2a0637ebac918699ba0cebe
korpi-zip <santiescoal@gmail.com>
2024-06-11 18:11:45 -0500

    add .env on git

--- config/db.py
+++ config/db.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL",default="sqlite:///./test.db")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL,echo=False)
 
